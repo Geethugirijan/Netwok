a9240e8a4386ef0cc5f2c6029f40f7abd7bac660
Geethugirijan <geethugirijan234@gmail.com>
2025-05-19 14:32:26 +0530

    Add files via upload

---
+++ eqornot.sh
@@ -0,0 +1,8 @@
+read -p "enter first number: " first
+read -p "enter second number: " second
+if [ $first -eq $second ]
+then
+echo "BOTH ARE EQUAL "
+else
+echo "NOT EQUAL "
+fi
---
+++ reverse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "enter a number: " num
+rev=0
+while [ $num -ne 0 ]
+do
+rem=$((num % 10))
+rev=$((rev * 10 + rem))
+num=$((num / 10))
+done
+echo "Reverse: $rev"
+
+
